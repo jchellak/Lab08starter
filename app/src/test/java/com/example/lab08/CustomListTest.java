def0ba8ffa8f9e9b3af58cf67f91652f0691c346
Joseph Chellakudam <jchellak@ualberta>
2026-03-03 17:43:27 -0700

    just a comment

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,6 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CustomListTest {
+public class CustomListTest { // just//
 
 }
